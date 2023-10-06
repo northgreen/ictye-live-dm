cf85ab2a052511b360d7219312d4c55a7a2bbd36
ictye <northgreen2006@qq.com>
2023-10-07 00:44:46 +0800

    初步完成前端插件接口的任务，不过还是有点问题难以解决

--- web/js/script/plugin_manager.js
+++ web/js/script/plugin_manager.js
@@ -4,14 +4,13 @@ require.config({
 define(
     function (){
         console.info("plugin system is ready")
-        let plugin_list = plugin_load()
-
+        //消息插件获取
         let plugin_load = function (){
             let get_plugin = new XMLHttpRequest()
             let plugin_list = []
 
             get_plugin.open("get","/api/plugin_list",false)
-
+            //回调
             get_plugin.onreadystatechange = function () {
                 if (get_plugin.readyState === XMLHttpRequest.DONE && get_plugin.status === 200) {
                     let req = JSON.parse(get_plugin.responseText)
@@ -19,29 +18,34 @@ define(
                 }
             }
             get_plugin.send()
-            /*
-            for(let a in plugin_list){
-                let plugin
-                require([plugin_list[a]],function (plug) {
-                    plugin = plug
-                })
-                plugins[plugin_list[a]] = plugin
-            }
-            console.info("az")
-            * */
-            console.info(plugin_list)
-
             return plugin_list
-            // TODO :剩余工作
         }
 
-        let message_hader = function (plugin_list) {
-            //TODO:消息处理器
+        let plugin_list = plugin_load()
+        //消息插件列表
+        let plugin_message_handler = {}
+        //消息插件注册
+        for (let x in plugin_list){
+            require([plugin_list[x]],function (plugin){
+                plugin_message_handler[plugin.message_class] = plugin.dm_halder
+            })
+        }
+
+        /*消息回调*/
+        let message_handler = function (message) {
+           if (Object.keys(plugin_message_handler).length !== 0){
+               plugin_message_handler[message.message_class](message)
+           }else {
+               // FIXME: 似乎有个难以解决的bug，不过貌似不是很重要，在插件加载之前的消息总是不能被正确处理
+               console.debug("a bug?")
+               console.debug(plugin_message_handler)
+           }
+
         }
 
         return {
             plugin_load: plugin_load,
-            message_halders: message_hader,
+            message_handlers: message_handler,
             plugin_list: plugin_list
         }
     }
