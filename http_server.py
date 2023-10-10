f684254907f6e1e0e4389cf991fcc12155d07bc6
ictye <northgreen2006@qq.com>
2023-10-10 15:49:41 +0800

    更新工作流

--- http_server.py
+++ http_server.py
@@ -3,9 +3,10 @@ import msgs
 import json
 import logging
 import os
+import pluginsystem
 
 config = dict()
-plugin_system: object
+plugin_system: pluginsystem.Plugin
 
 
 async def http_handler(request):  # 主文件请求
@@ -20,16 +21,21 @@ async def http_socket_get(request):
     return web.Response(text=json.dumps(msgs.socket_responce(config).to_dict()))
 
 
-async def http_websocket(request):
+async def http_websocket(request: web.Request):
     ws = "ws://{}:{}".format(config["host"], config["websocket"]["port"])
     return web.WebSocketResponse
 
 
-async def http_plugin(request):
+async def http_plugin(request: web.Request):
     log = logging.getLogger(__name__)
     log.info(f"request for {request.match_info['name']}")
-    # TODO(ictye):实现软js插件
-    return web.FileResponse(path=f"web/js/plugin/{request.match_info['name']}")
+    if os.path.exists(f"web/js/plugin/{request.match_info['name']}"):
+        return web.FileResponse(path=f"web/js/plugin/{request.match_info['name']}")
+    elif request.match_info["name"] in plugin_system.plugin_js_support:
+        return web.Response(text=plugin_system.plugin_js_support[request.match_info["name"]],
+                            content_type="application/javascript")
+    else:
+        return web.Response(status=404, text="not found")
 
 
 async def http_style(request):
@@ -59,10 +65,10 @@ async def http_script(request):
 async def http_api_plugin(request):
     log = logging.getLogger(__name__)
     log.info(f"request for web plugin list")
-    # TODO(ictye): 实现软js插件
+
     plugin_list = {"code": 200,
                    "list": [os.path.splitext(file_name)[0] for file_name in os.listdir("./web/js/plugin") if
-                            file_name.endswith('.js')]}
+                            file_name.endswith('.js')] + list(plugin_system.plugin_js_support)}
 
     return web.json_response(plugin_list)
 
