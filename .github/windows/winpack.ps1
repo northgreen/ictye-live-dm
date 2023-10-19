f40dce159443fdef996b5a6b5660b8536422e82d
ictye <northgreen2006@qq.com>
2023-10-19 22:10:04 +0800

    测试打包

--- .github/windows/winpack.ps1
+++ .github/windows/winpack.ps1
@@ -4,6 +4,7 @@ Write-Output $work_path
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.6/python-3.11.6-embed-amd64.zip" -OutFile "$work_path/python.zip"
 mkdir $work_path/ictye-live-dm/bin
 Expand-Archive -LiteralPath "$work_path/python.zip" -DestinationPath "$work_path/ictye-live-dm/bin"
+&"$work_path/ictye-live-dm/bin/python.exe" "$work_path/ictye-live-dm/bin/get-pip.py"
 
 $file_path = Join-Path $work_path "ictye-live-dm/bin/python38._pth"
 $lines = Get-Content -Path $file_path
@@ -19,7 +20,7 @@ foreach ($line in $lines) {
 
 $updatedLines | Set-Content -Path $file_path
 
-&"$work_path/ictye-live-dm/bin/python.exe" "$work_path/ictye-live-dm/bin/get-pip.py"
+
 
 Write-Output @Set PATH=%%~dp0:%%~dp0Scripts:%%PATH%% > $work_path/ictye-live-dm/run.bat 
 Write-Output @Set root=%%~dp0 >> $work_path/ictye-live-dm/run.bat
