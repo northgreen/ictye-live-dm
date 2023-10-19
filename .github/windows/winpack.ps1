afba133dd85797a69b8085f28bce6c22a87d0574
ictye <northgreen2006@qq.com>
2023-10-19 21:57:06 +0800

    测试打包

--- .github/windows/winpack.ps1
+++ .github/windows/winpack.ps1
@@ -1,6 +1,9 @@
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.6/python-3.11.6-embed-amd64.zip" -OutFile "$GITHUB_WORKSPACE/python.zip"
-mkdir $GITHUB_WORKSPACE/ictye-live-dm/bin
-Expand-Archive -LiteralPath "$GITHUB_WORKSPACE/python.zip" -DestinationPath "$GITHUB_WORKSPACE/ictye-live-dm/bin"
-Get-ChildItem $GITHUB_WORKSPACE
-Get-ChildItem $GITHUB_WORKSPACE/ictye-live-dm/
-tree /f $GITHUB_WORKSPACE
+$work_path = $PWD.Path
+Write-Output $work_path
+
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.6/python-3.11.6-embed-amd64.zip" -OutFile "$work_path/python.zip"
+mkdir $work_path/ictye-live-dm/bin
+Expand-Archive -LiteralPath "$work_path/python.zip" -DestinationPath "$work_path/ictye-live-dm/bin"
+Get-ChildItem $work_path
+Get-ChildItem $work_path/ictye-live-dm/
+tree /f $work_path
