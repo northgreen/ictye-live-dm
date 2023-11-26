05710fea8c90f7d98627d229deb5b34530003fb9
ictye <northgreen2006@qq.com>
2023-11-26 20:25:26 +0800

    简化代码和提高实用性

--- .github/resource/run.bat
+++ .github/resource/run.bat
@@ -1,7 +1,3 @@
-@Set PATH=%~dp0:%~dp0Scripts:%PATH%
-@Set root=%~dp0
-@Set root=%root:\=\\%
-@(Set /p=%root%) <NUL> %~dp0\lib\site-package\WindPy.pth
 "./bin/python.exe" -m pip install -r ./requirements.txt
 "./bin/python.exe" ./
 pause
