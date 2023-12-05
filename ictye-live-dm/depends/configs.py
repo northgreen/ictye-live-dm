2c9b58f2dc4c7e8f0e497be08a6f6061166899b0
ictye <northgreen2006@qq.com>
2023-12-05 22:23:44 +0800

    為發佈做準備

--- ictye-live-dm/depends/configs.py
+++ ictye-live-dm/depends/configs.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 
 def config() -> dict:
@@ -22,11 +23,19 @@ def set_config(config_family: str, config: dict) -> bool:
     finally:
         return True
 
+
 def read_config(config_family: str) -> dict:
     """
     读取插件的配置
     """
-    with open(f"./config/plugin/{config_family}/config.yaml", "r", encoding="utf_8") as f:
-        configs = yaml.load(f.read(), Loader=yaml.FullLoader)
-    return configs
-
+    print(os.path.abspath(f"./config/plugin/{config_family}/config.yaml"))
+    configs = {}
+    if os.path.exists(f"./config/plugin/{config_family}/config.yaml"):
+        with open(f"./config/plugin/{config_family}/config.yaml", "r", encoding="utf_8") as f:
+            configs = yaml.load(f.read(), Loader=yaml.FullLoader)
+            return configs
+    else:
+        os.makedirs(os.path.dirname(f"./config/plugin/{config_family}/config.yaml"),exist_ok=True)
+        with open(f"./config/plugin/{config_family}/config.yaml", 'w+') as f:
+            f.write(f"# config for {config_family}")
+        return configs
