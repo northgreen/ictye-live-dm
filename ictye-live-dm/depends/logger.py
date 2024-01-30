f1aee83dede2848539af9050a5be640db9ef277a
ictye <northgreen2006@qq.com>
2024-01-30 15:19:10 +0800

    完善日志系统，增加并完善一个api

--- ictye-live-dm/depends/logger.py
+++ ictye-live-dm/depends/logger.py
@@ -1,4 +1,6 @@
 import logging
+import datetime
+import os
 
 
 def setup_logging(config: dict):
@@ -12,4 +14,29 @@ def setup_logging(config: dict):
                        "ERROR": logging.ERROR,
                        "CRITICAL": logging.CRITICAL,
                        "FATAL": logging.FATAL}
-    logging.basicConfig(level=level_dic[config["loglevel"]], format="[%(asctime)s,%(name)s] %(levelname)s : %(message)s")
+
+    logger = logging.getLogger()  # 获取全局logger
+    logger.setLevel(level_dic[config["loglevel"]])  # 设置日志级别
+
+    # 创建一个handler，用于写入日志文件
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    fh = logging.FileHandler(
+        os.path.join('logs', config["logfile"]["name"] + datetime.time().strftime("%Y%m%d_%H%M%S") + ".log"),
+        encoding="utf-8")
+    fh.setLevel(level_dic[config["loglevel"]])
+
+    # 创建一个handler，用于将日志输出到控制台
+    ch = logging.StreamHandler()
+    ch.setLevel(level_dic[config["loglevel"]])
+
+    # 定义handler的输出格式
+    formatter = logging.Formatter("[%(asctime)s,%(name)s] %(levelname)s : %(message)s")
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    # 给logger添加handler
+    if config["logfile"]["open"]:
+        logger.addHandler(fh)
+    logger.addHandler(ch)
