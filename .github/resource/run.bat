e803e4dd4c1f1d929caad7eb0279e5849458e0e5
ictye <95436661+northgreen@users.noreply.github.com>
2024-02-02 17:22:24 +0800

    完善啓動脚本 (#20)
    * 完善啓動脚本

--- .github/resource/run.bat
+++ .github/resource/run.bat
@@ -1,3 +1,17 @@
+@echo off
+echo installing require
 "./bin/python.exe" -m pip install -r ./requirements.txt
+
+for /R ./plugin %%f in (requirements.txt) do (
+if exist %%f (
+        echo %%~dpf | findstr /I /V /C:"\\__pycache__\\" 1>nul
+        if not errorlevel 1 (
+            echo Installing packages from: %%f
+            "./bin/python.exe" -m pip install -r  "%%f"
+        )
+    )
+)
+
+echo starting
 "./bin/python.exe" ./
 pause
