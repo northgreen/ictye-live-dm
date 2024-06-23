c9d7ac1e1bbf92ba99e71b7ee9bacbc8352b4c4d
ictye <northgreen2006@qq.com>
2024-06-23 17:54:22 +0800

    docs: 起草文檔而已

---
+++ ictye-live-dm/translate/zh-ch_MainUI.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="zh_TW">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="127"/>
+        <source>Ictye Live Danmmku</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="128"/>
+        <source>本項目由美國聖地亞哥（American Sangdiyagou）獨家贊助研發(x)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="129"/>
+        <source>Start</source>
+        <translatorcomment>启动服务器</translatorcomment>
+        <translation>启动</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="130"/>
+        <source>Stop</source>
+        <translatorcomment>停止服务器</translatorcomment>
+        <translation>停止</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="131"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="132"/>
+        <source>Follow newest log</source>
+        <translation>自动滚动</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="135"/>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="137"/>
+        <source>Level</source>
+        <translation>等级</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="139"/>
+        <source>Contents</source>
+        <translation>内容</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="140"/>
+        <source>Log</source>
+        <translation>日志</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="141"/>
+        <source>Total:</source>
+        <translation>总计</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="142"/>
+        <source>Submit</source>
+        <translation>注册</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="144"/>
+        <source>New Column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="146"/>
+        <source>Plugin name</source>
+        <translation>插件名称</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="148"/>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="149"/>
+        <source>Plugins</source>
+        <translation>插件</translation>
+    </message>
+    <message>
+        <location filename="../src/ictye_live_dm/GUI/Ui_MainWindow.py" line="150"/>
+        <source>Setting</source>
+        <translation>设置</translation>
+    </message>
+</context>
+</TS>
